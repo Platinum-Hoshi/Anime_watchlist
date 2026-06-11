70c9c7ab22aa226b44296d5363d6e08dbdc1e64d
Platinum-Hoshi <levin.britt@outlook.com>
2026-06-11 11:11:26 +0200

    Change CORS settings to allow all origins
    Updated CORS settings to allow all origins.

--- Backend/main.py
+++ Backend/main.py
@@ -27,7 +27,7 @@ def startup():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
